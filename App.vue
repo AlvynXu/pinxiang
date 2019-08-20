2360e8ea24e4c83f985f0b39fa9867b369ed8fe1
Alvyn <1529634106@qq.com>
2019-08-20 11:21:38 +0800

    添加配置

--- App.vue
+++ App.vue
@@ -2,6 +2,15 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			wx.checkSession({
+			  success () {
+			    //session_key 未过期，并且在本生命周期一直有效
+			  },
+			  fail () {
+			    // session_key 已经失效，需要重新执行登录流程
+			    wx.login() //重新登录
+			  }
+			})
 		},
 		onShow: function() {
 			console.log('App Show')
