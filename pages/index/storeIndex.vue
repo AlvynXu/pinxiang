7a0452cda666ddaf76a7832adbc1f52f397182d0
unknown <1476308572@qq.com>
2019-11-09 16:10:25 +0800

    修改商铺的首页页面

--- pages/index/storeIndex.vue
+++ pages/index/storeIndex.vue
@@ -4,27 +4,40 @@
 			<view class="status_bar">
 				<!-- 这里是状态栏 -->
 			</view>
-			<view class="header-box-title" :style="{bottom:bottom}">品象养车</view>
+			<view class="header-box-title">品象养车</view>
+			<!-- <view class="header-box-title" :style="{bottom:bottom}">品象养车</view>
 			<view class="header-address-box" @tap="chooseArea" :style="{bottom:areaBottom}">
 				<text class="vant-icon header-address-icon">&#xe68f;</text>
 				<text class="header-address-text">{{area}}</text>
+			</view> -->
+			<view class="address-search">
+				<view class="address-box" @tap="chooseArea" :style="{bottom:areaBottom}">
+					<text class="vant-icon address-icon">&#xe68f;</text>
+					<text class="address-text">{{area}}</text>
+				</view>
+				<view class="store-search" @tap="goSearch">
+					<view class="store-search-input">
+						<view class="input">输入门店信息</view>
+					</view>
+					<view class="vant-icon">&#xe692;</view>
+				</view>
 			</view>
 		</view>
-		<view style="height: 158upx;"></view>
+		<view style="height: 220upx;"></view>
 		<view class="store-swiper">
-			<swiper class="swiper" circular="true" :current="current" :autoplay="autoplay" :interval="interval" :duration="duration" :circular="circular">
+			<swiper class="swiper" circular="true" :indicator-dots="true" indicator-color="#FFFFFF" indicator-active-color="#FE5100" :current="current" :autoplay="autoplay" :interval="interval" :duration="duration">
 				<swiper-item v-for="(val,key) in swiperList" :key="key">
 					<image class="swiper-img" :src="val.Image" mode="aspectFill" :lazy-load="true"></image>
 				</swiper-item>
 			</swiper>
-			<view class="store-search" @tap="goSearch">
+			<!-- <view class="store-search" @tap="goSearch">
 				<view class="store-search-input">
 					<view class="input">输入门店信息</view>
 				</view>
 				<view class="vant-icon">&#xe692;</view>
-			</view>
+			</view> -->
 		</view>
-		<view class="store-vip" @tap="goActive">
+		<!-- <view class="store-vip" @tap="goActive">
 			<view class="vip-top">
 				<view class="vip-name">立即开卡</view>
 				<view class="vant-icon">&#xe65c;</view>
@@ -32,6 +45,34 @@
 			<view class="vip-bottom">
 				<view class="vip-name">全年免费保养，另送12次洗车服务</view>
 				<view class="vant-icon">&#xe69e;</view>
+			</view>
+		</view> -->
+		<!-- 立即开会员开 -->
+		<view class="vip-box">
+			<view class="vip-box-card"  @tap="goActive" >
+				<image src="https://cdn.doudouxiongglobal.com/pinxiang/V1.2/vipbg-13.png"></image>
+				<view class="position">
+					<view class="position-text">会员养车·全年免费</view>
+					<view class="position-text-white">立即开卡，赠送12次洗车服务</view>
+				</view>
+			</view>
+			<view class="vip-card-service">
+				<view>
+					<view class="vant-icon circular">&#xe6ad;</view>
+					<text>严选门店</text>
+				</view>
+				<view>
+					<view class="vant-icon circular">&#xe6ab;</view>
+					<text>会员畅养</text>
+				</view>
+				<view>
+					<view class="vant-icon circular">&#xe6ae;</view>
+					<text>洗车免费</text>
+				</view>
+				<view>
+					<view class="vant-icon circular">&#xe6bf;</view>
+					<text>品质保证</text>
+				</view>
 			</view>
 		</view>
 		<view class="gray"></view>
@@ -58,14 +99,19 @@
 				</view>
 				<view class="store-name">{{val.Name}}</view>
 				<view class="store-distance">{{val.Distance}}</view>
-				<view class="store-address">{{val.Address}}
-					<uni-rate :value="val.Rate" size="13" :disabled="true"></uni-rate>
+				<view class="store-address">
+				    <view>{{val.Address}}</view>
+				    <view class="appointment-number">已预:{{val.AppointCount}}</view>
 				</view>
 				<view class="store-tips">
-					<view class="store-tip-list">
+					<!-- <view class="store-tip-list">
 						<view class="tips" v-for="(v,k) in val.TagsZH" :key="k">
 							<view class="tip">{{v}}</view>
 						</view>
+					</view> -->
+					<view class="rate">
+						<uni-rate :value="val.Rate" size="13" :disabled="true"></uni-rate>
+						<view class="per-capita-price">¥ {{val.Average}}/人</view>
 					</view>
 					<view class="store-buy" v-if="vip===0">会员免费</view>
 					<view class="store-buy" v-if="vip===1">立即预约</view>
@@ -83,14 +129,20 @@
 				</view>
 				<view class="store-name">{{val.Name}}</view>
 				<view class="store-distance">{{val.Distance}}</view>
-				<view class="store-address">{{val.Address}}
-					<uni-rate :value="val.Rate" size="13" :disabled="true"></uni-rate>
+				<view class="store-address">
+				    <view>{{val.Address}}</view>
+					<view class="appointment-number">已预约:{{val.AppointCount}}</view>
+					<!-- <uni-rate :value="val.Rate" size="13" :disabled="true"></uni-rate> -->
 				</view>
 				<view class="store-tips">
-					<view class="store-tip-list">
+					<!-- <view class="store-tip-list">
 						<view class="tips" v-for="(v,k) in val.TagsZH" :key="k">
 							<view class="tip">{{v}}</view>
 						</view>
+					</view> -->
+					<view class="rate">
+						<uni-rate :value="val.Rate" size="13" :disabled="true"></uni-rate>
+						<view class="per-capita-price">¥ 430/人</view>
 					</view>
 					<view class="store-buy" v-if="vip===0">会员免费</view>
 					<view class="store-buy" v-if="vip===1">立即预约</view>
@@ -359,45 +411,58 @@
 		top:0;
 		left:0;
 		width: 750upx;
-		height: 154upx;
 		background: #FCFCFC;
 		overflow: hidden;
 		z-index:100;
-	}
-	.header-box-title{
-		position: absolute;
-		width: 100%;
-		line-height: 58upx;
-		font-size:33upx;
-		font-family:PingFangSC;
-		font-weight:400;
-		color:rgba(51,51,51,1);
-		text-align: center;
-		left:0;
-		z-index:101;
-	}
-	.header-address-box{
-		position: absolute;
-		left:20upx;
-		z-index: 102;
-		.header-address-icon{
+		.header-box-title{
+			width: 100%;
+			line-height: 78upx;
 			font-size:33upx;
-			color:#A4A4A4;
-			vertical-align: middle;
+			font-family:PingFangSC;
+			font-weight:400;
+			color:rgba(51,51,51,1);
+			text-align: center;
 		}
-		.header-address-text{
-			display:inline-block;
-			margin-left:-10upx;
-			font-size:30upx;
-			line-height: 30upx;
-			width: 150upx;
-			overflow: hidden;/*超出部分隐藏*/
-			text-overflow:ellipsis;/* 超出部分显示省略号 */
-			white-space: nowrap;/*规定段落中的文本不进行换行 */
-			color:#A4A4A4;
-			vertical-align: middle;
+		.address-search{
+			height: 80upx;
+			padding: 0 40upx;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			.address-box{
+				.address-icon{
+					font-size:33upx;
+					color:#A4A4A4;
+					vertical-align: middle;
+				}
+				.address-text{
+					display:inline-block;
+					margin-left:-10upx;
+					font-size:30upx;
+					line-height: 30upx;
+					width: 150upx;
+					overflow: hidden;/*超出部分隐藏*/
+					text-overflow:ellipsis;/* 超出部分显示省略号 */
+					white-space: nowrap;/*规定段落中的文本不进行换行 */
+					color:#A4A4A4;
+					vertical-align: middle;
+				}
+			}
+			.store-search{
+				width:494upx;
+				height:54upx;
+				border-radius:11upx;
+				background:rgba(164,164,164,0.2);
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				padding: 0 12upx 0 22upx;
+				color:#A4A4A4;
+				font-size: 25upx;
+			}
 		}
 	}
+
 	.store-box {
 		display: flex;
 		flex-direction: column;
@@ -407,86 +472,119 @@
 		margin-bottom:10upx;
 		.store-swiper{
 			width:750upx;
-			height:413upx;
+			height: 384upx;
 			position: relative;
 			.swiper{
 				width:750upx;
-				height:413upx;
+				height:384upx;
 				.swiper-img{
 					width:750upx;
-					height:413upx;
+					height:384upx;
 				}
 			}
-			.store-search{
-				width:667upx;
-				height:121upx;
-				background: #fff;
-				position: absolute;
-				top:353upx;
-				left:42upx;
+		}
+		.vip-box{
+			width:750upx;
+			box-sizing: border-box;
+			padding: 36upx 42upx 0 42upx;
+			background:#FFFFFF;
+			.vip-box-card{
+				position: relative;
+				width:666upx;
+				height:152upx;
 				border-radius:11upx;
-				box-shadow: 0px 0px 3px 3px rgba(164,164,164,0.1);
-				display: flex;
-				justify-content: space-between;
-				.store-search-input{
-					margin-top: 45upx;
-					width:450upx;
-					margin-left:33upx;
-					.input{
+				.position{
+					position: absolute;
+					top: 0;
+					left: 0;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					flex-direction: column;
+					width:666upx;
+					height:152upx;
+					.position-text{
+						font-size:36upx;
+						font-weight:500;
+						color:rgba(75,42,0,1);
+						line-height:50upx;
+					}
+					.position-text-white{
 						font-size:25upx;
-						color:#A4A4A4;
+						font-weight:500;
+						color:rgba(252,245,212,1);
+						line-height:50upx;
 					}
 				}
-				.vant-icon{
-					margin-top: 45upx;
-					font-size:30upx;
-					margin-right: 60upx;
-					color:#FE5100;
+				image{
+					width:666upx;
+					height:152upx;
+				}
+			}
+			.vip-card-service{
+				height: 98upx;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				font-size: 25upx;
+				font-weight:400;
+				color:rgba(51,51,51,1);
+				line-height:36upx;
+				.circular{
+					border-radius: 50%;
+					line-height: 36upx;
+					text-align: center;
+					width:36upx;
+					height:36upx;
+					color: #FFFFFF;
+					font-size: 20upx;
+					display: inline-block;
+					background:linear-gradient(180deg,rgba(254,140,0,1) 0%,rgba(254,81,0,1) 100%);
 				}
 			}
 		}
-		.store-vip{
-			width:667upx;
-			height:134upx;
-			box-shadow: 0px 0px 3px 3px rgba(164,164,164,0.1);
-			margin: 0 auto;
-			margin-top: 100upx;
-			margin-bottom: 15upx;
-			border-radius:11upx;
-			.vip-top{
-				width:580upx;
-				font-size:29upx;
-				color:#FE5100;
-				display: flex;
-				height:65upx;
-				line-height:65upx;
-				margin-left:33upx;
-				justify-content: space-between;
-				border-bottom:1upx solid rgba(254,81,0,0.1);
-				.vant-icon{
-					font-size:40upx;
-					margin-right: 0;
-				}
-			}
-			.vip-bottom{
-				width:580upx;
-				font-size:22upx;
-				display: flex;
-				height:65upx;
-				line-height:65upx;
-				margin-left:33upx;
-				justify-content: space-between;
-				.vip-name{
-					color:#A4A4A4;
-				}
-				.vant-icon{
-					font-size:40upx;
-					margin-right: 0;
-					margin-top:2upx;
-					color:rgba(254,81,0,0.5)
-				}
-			}
-		}
+		// .store-vip{
+		// 	width:667upx;
+		// 	height:134upx;
+		// 	box-shadow: 0px 0px 3px 3px rgba(164,164,164,0.1);
+		// 	margin: 0 auto;
+		// 	margin-top: 100upx;
+		// 	margin-bottom: 15upx;
+		// 	border-radius:11upx;
+		// 	.vip-top{
+		// 		width:580upx;
+		// 		font-size:29upx;
+		// 		color:#FE5100;
+		// 		display: flex;
+		// 		height:65upx;
+		// 		line-height:65upx;
+		// 		margin-left:33upx;
+		// 		justify-content: space-between;
+		// 		border-bottom:1upx solid rgba(254,81,0,0.1);
+		// 		.vant-icon{
+		// 			font-size:40upx;
+		// 			margin-right: 0;
+		// 		}
+		// 	}
+		// 	.vip-bottom{
+		// 		width:580upx;
+		// 		font-size:22upx;
+		// 		display: flex;
+		// 		height:65upx;
+		// 		line-height:65upx;
+		// 		margin-left:33upx;
+		// 		justify-content: space-between;
+		// 		.vip-name{
+		// 			color:#A4A4A4;
+		// 		}
+		// 		.vant-icon{
+		// 			font-size:40upx;
+		// 			margin-right: 0;
+		// 			margin-top:2upx;
+		// 			color:rgba(254,81,0,0.5)
+		// 		}
+		// 	}
+		// }
 		.store-nearby{
 			width:667upx;
 			height:107upx;
@@ -544,39 +642,59 @@
 				margin-top:18upx;
 				display: flex;
 				justify-content: space-between;
-				.uni-rate{
-					margin-top:10upx;
+				.appointment-number{
+					font-size: 25upx;
+					font-weight:400;
+					color: #FE8C00;
 				}
 			}
+			
 			.store-tips{
 				width: 667upx;
 				margin: 0 auto;
 				margin-top: 17upx;
 				display: flex;
-				justify-content:flex-start;
+				align-items: center;
+				justify-content:space-between;
 				padding-bottom:20upx;
-				.store-tip-list{
-					width:470upx;
-					display: flex;
-					justify-content:flex-start;
+				.rate{
+					position: relative;
+					width: 266upx;
+					.per-capita-price{
+						position: absolute;
+						right: 0;
+						top:50%;
+						transform: translateY(-50%);
+						font-size:22upx;
+						font-weight:400;
+						color:rgba(164,164,164,1);
+					}
+				}
+				.uni-rate{
 					margin-top:10upx;
-					.tips{
+				}
+				// .store-tip-list{
+				// 	width:470upx;
+				// 	display: flex;
+				// 	justify-content:flex-start;
+				// 	margin-top:10upx;
+					// .tips{
 						// margin-top: 10upx;	
 						// display: flex;
 						// justify-content:flex-start;
-						.tip{
-							width:141upx;
-							height:40upx;
-							margin-right: 5upx;
-							line-height: 40upx;
-							text-align: center;
-							color:#FE8C00;
-							font-size: 25upx;
-							border-radius:18px;
-							border:2upx solid rgba(254,140,0,1);
-						}
-					}
-				}
+				// 		.tip{
+				// 			width:141upx;
+				// 			height:40upx;
+				// 			margin-right: 5upx;
+				// 			line-height: 40upx;
+				// 			text-align: center;
+				// 			color:#FE8C00;
+				// 			font-size: 25upx;
+				// 			border-radius:18px;
+				// 			border:2upx solid rgba(254,140,0,1);
+				// 		}
+				// 	}
+				// }
 				.store-buy{
 					width:163upx;
 					height:67upx;
